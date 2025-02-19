00d6f4a7c365bda545a959ace86f28e079fd5bd2
MD Abir mahmud <abirmahmud5665@gmail.com>
2025-02-19 12:20:59 +0600

    done : add new file Car

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using practice.Properties;
 
 namespace practice
 {
@@ -10,6 +12,8 @@ namespace practice
     {
         static void Main(string[] args)
         {
+            Car myCar = new Car();
+            Console.WriteLine(myCar.color.ToUpper());
         }
     }
 }
---
+++ Properties/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice.Properties
+{
+    internal class Car
+    {
+        public string color = "red";
+    }
+}
